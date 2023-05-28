b0e3a5ce4010c091ce501767ae9480e1b3955695
Soham Datta <sohamdatta34@gmail.com>
2023-05-28 18:08:08 +0530

    new logo, added styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,9 +8,10 @@ const Navbar: React.FC<NavbarProps> = () => {
     return (
         <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
             <Link href="/" className="flex items-center justify-center h-20">
-                <img src="/logo.svg" alt="EliteCode" className="h-10" />
+                <img src="/logo.png" alt="EliteCode" className="h-10" />
             </Link>
-            <button className="bg-brand-orange px-2 py-1 sm:px-4 rounded-lg">Sign In</button>
+            <button className="bg-brand-orange px-2 py-1 sm:px-4 rounded-lg text-white text-sm font-medium
+            hover:bg-white hover:text-brand-orange transition duration-300">Sign In</button>
         </div >
     )
 }
