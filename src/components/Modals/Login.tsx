53d318ae7d1d2aaa3d3f16f9a30e69e3a5def8e8
Soham Datta <sohamdatta34@gmail.com>
2023-05-29 22:18:10 +0530

    added forgor pass and link to signup

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -2,12 +2,12 @@ interface Props { };
 
 const Login: React.FC<Props> = () => {
     return (
-        <form action="submit" className="space-y-4 px-4 pb-4">
+        <form className="space-y-4 px-4 pb-4">
             <h1 className="text-center text-2xl text-white font-medium">Sign in to EliteCode</h1>
             <div>
                 <label htmlFor="email" className="my-2 block text-white">Email</label>
                 <input type="email" name="email" id="email" placeholder="example@mail.com"
-                    className="p-1.5 text-black sm:text-sm placeholder-gray-400 rounded-md outline-none border-5 w-full"
+                    className="p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400 "
                 />
             </div>
             <div>
@@ -17,6 +17,10 @@ const Login: React.FC<Props> = () => {
                 />
             </div>
             <button type="submit" className="py-2 w-full rounded-md bg-brand-orange font-medium text-white hover:bg-white hover:text-brand-orange transition duration-300">Log in</button>
+            <button className="w-full text-xs text-end text-white hover:text-brand-orange hover:underline">Forgot password?</button>
+            <div>
+                <p className="text-center text-white">Don't have an account? <a href="#" className="text-brand-orange hover:underline">Sign up</a></p>
+            </div>
         </form>
     )
 }
