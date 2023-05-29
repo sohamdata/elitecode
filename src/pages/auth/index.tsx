38948b07d7982f841984aa6a1446f183e5dc24c8
Soham Datta <sohamdatta34@gmail.com>
2023-05-29 16:24:06 +0530

    added hero img

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -6,10 +6,12 @@ type AuthPageProps = {}
 const AuthPage: React.FC<AuthPageProps> = () => {
     return (
         <div className="bg-gradient-to-b from-gray-500 to-black h-screen">
-            <div className="mx-auto max-w-7xl">
-                <Navbar />
+            <Navbar />
+            <div className="flex flex-col justify-center items-center h-5/6 pointer-events-none select-none">
+                <img src="/hero.png" alt="AC" style={{ transform: "scale(0.7)" }} />
             </div>
         </div>
+
     )
 }
 
