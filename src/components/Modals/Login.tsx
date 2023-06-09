717cf4b0768d06b279817cd037033599d4af78f5
Soham Datta <sohamdatta34@gmail.com>
2023-06-09 14:14:05 +0530

    added functional firebase signin

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,9 +1,18 @@
 import { authModalState } from '@/atoms/authModalAtom';
 import { useSetRecoilState } from 'recoil';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/firebase';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 interface Props { };
 
 const Login: React.FC<Props> = () => {
+    const router = useRouter();
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [signInWithEmailAndPassword, user, loading, error,] = useSignInWithEmailAndPassword(auth);
+
     const setAuthModalState = useSetRecoilState(authModalState);
 
     const handleClickSignup = () => {
@@ -14,23 +23,59 @@ const Login: React.FC<Props> = () => {
         setAuthModalState((prev) => ({ ...prev, mode: "forgotPassword" }));
     }
 
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        switch (name) {
+            case "password":
+                setPassword(value);
+                break;
+            case "email":
+                setEmail(value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!password || !email) return alert("we need all fields to send you spam mails.");
+        try {
+            const newUser = await signInWithEmailAndPassword(email, password);
+            if (!newUser) return;
+            router.push("/");
+        } catch (error: any) {
+            alert(error.message);
+        }
+    }
+
+    useEffect(() => {
+        if (error) {
+            alert(error.message);
+        }
+    }, [error]);
+
     return (
-        <form className="space-y-4 px-4 pb-4">
+        <form className="space-y-4 px-4 pb-4" onSubmit={handleSignIn}>
             <h1 className="text-center text-2xl text-white font-medium">Sign in to EliteCode</h1>
             <div>
                 <label htmlFor="email" className="my-2 block text-white">Email</label>
                 <input type="email" name="email" id="email" placeholder="example@mail.com"
-                    className="p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400 "
+                    className="p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400"
+                    onChange={handleInputChange}
                 />
             </div>
             <div>
                 <label htmlFor="password" className="my-2 block text-white">Password</label>
                 <input type="password" name="password" id="password" placeholder="a uniquely insecure password"
-                    className="mb-4 p-1.5 text-black sm:text-sm placeholder-gray-400 rounded-md outline-none border-5 w-full"
+                    className="mb-4 p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400"
+                    onChange={handleInputChange}
                 />
             </div>
-            <button type="submit" className="py-2 w-full rounded-md bg-brand-orange font-medium text-white hover:bg-white hover:text-brand-orange transition duration-300">Log in</button>
-            <button className="w-full text-xs text-end text-white hover:text-brand-orange hover:underline"
+            <button type="submit" className="py-2 w-full rounded-md bg-brand-orange font-medium text-white hover:bg-white hover:text-brand-orange transition duration-300">
+                {loading ? "Loading..." : "Sign in"}
+            </button>
+            <button type="submit" className="w-full text-xs text-end text-white hover:text-brand-orange hover:underline"
                 onClick={handleClickForgotPassword}
             >Forgor password?</button>
             <div>
