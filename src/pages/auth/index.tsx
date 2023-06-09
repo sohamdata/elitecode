2b9267d860d376bf99f2ec15cd72d0254a35e27a
Soham Datta <sohamdatta34@gmail.com>
2023-06-09 14:35:53 +0530

    init auth page session handling

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,29 @@
 import AuthModal from '@/components/Modals/AuthModal'
 import Navbar from '@/components/Navbar/Navbar'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { authModalState } from '@/atoms/authModalAtom'
 import { useRecoilValue } from 'recoil'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '@/firebase/firebase'
+import { useRouter } from 'next/router'
 
 interface AuthPageProps { };
 
 const AuthPage: React.FC<AuthPageProps> = () => {
+    const router = useRouter();
     const authModal = useRecoilValue(authModalState);
+    const [user, loading, error] = useAuthState(auth);
+    const [pageLoading, setPageLoading] = useState(true);
+
+    useEffect(() => {
+        if (user) {
+            router.push("/");
+        }
+        if (!user && !loading) { setPageLoading(false) }
+    }, [user, router, loading])
+
+    if (pageLoading) return null;
+
     return (
         <div className="bg-gradient-to-b from-gray-500 to-black h-screen">
             <Navbar />
