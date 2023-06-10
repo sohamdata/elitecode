e018af4c3b69c5a696f5f84a2ff0e54452842641
Soham Datta <sohamdatta34@gmail.com>
2023-06-10 13:11:42 +0530

    added difficulty color and yt icon

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,11 +1,12 @@
 import { MockProblems } from "@/MockProblems";
 import { BsCheckCircle } from "react-icons/bs";
+import { AiFillYoutube } from "react-icons/ai";
 
 type Props = {};
 
 const Table: React.FC<Props> = () => {
     return (
-        <table className='w-full text-left max-w-[1200px] mx-auto text-sm text-gray-400'>
+        <table className='w-full max-w-[1200px] text-left mx-auto text-sm text-gray-400'>
             <caption className='mb-4 text-2xl font-medium'>PROBLEMS</caption>
             <thead className='border-b uppercase'>
                 <tr>
@@ -19,11 +20,11 @@ const Table: React.FC<Props> = () => {
             <tbody>
                 {MockProblems.map((problem, index) => (
                     <tr key={problem.id} className='border-b'>
-                        <td className="py-4 pl-7 text-green-700"><BsCheckCircle size={20} /></td>
-                        <td className="py-4"><a href={`https://leetcode.com/problems/${problem.id}`} target="_blank" rel="noopener noreferrer">{problem.title}</a></td>
-                        <td className="py-4">{problem.difficulty}</td>
-                        <td className="py-4">{problem.category}</td>
-                        <td className="py-4">{!problem.videoId ? 'Coming Soon' : <a href={`https://www.youtube.com/watch?v=${problem.videoId}`} target="_blank" rel="noopener noreferrer">Solution</a>}</td>
+                        <td className="px-4 py-4 pl-7 text-green-700"><BsCheckCircle size={20} /></td>
+                        <td className="px-4 py-4 hover:text-sky-500"><a href={`https://leetcode.com/problems/${problem.id}`} target="_blank" rel="noopener noreferrer">{problem.title}</a></td>
+                        <td className={`px-4 py-4 ${problem.difficulty === "Easy" ? "text-green-600" : problem.difficulty === "Medium" ? "text-orange-500" : "text-red-500"}`}>{problem.difficulty}</td>
+                        <td className="px-4 py-4">{problem.category}</td>
+                        <td className="px-4 py-4">{!problem.videoId ? 'Coming Soon' : <a href={`https://www.youtube.com/watch?v=${problem.videoId}`} target="_blank" rel="noopener noreferrer"><AiFillYoutube size={20} className="hover:text-red-600" /></a>}</td>
                     </tr>
                 ))}
 
