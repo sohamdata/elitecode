7850853bdcb06fe653e7ec68acb3701a2d6c12a6
Soham Datta <sohamdatta34@gmail.com>
2023-06-15 22:59:35 +0530

    yt modal complete - styling god

--- src/components/Modals/YoutubeVideo.tsx
+++ src/components/Modals/YoutubeVideo.tsx
@@ -10,8 +10,8 @@ const YoutubeVideo: React.FC<Props> = ({ videoId, onClose }) => {
     return (
         <>
             <div className='fixed inset-0 flex items-center justify-center bg-black opacity-50' onClick={onClose}></div>
-            <div className='w-full sm:w-[800px] absolute top-[50%] left-[50%] transform translate-x-[-50%] -translate-y-[-50%]'>
-                <div className='rounded-lg shadow-lg relative'>
+            <div className='fixed inset-0 flex items-center justify-center'>
+                <div className='w-full sm:w-[800px]'>
                     <div className='flex justify-end p-2'>
                         <button
                             type='button'
