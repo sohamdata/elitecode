ee3e2e60c3d84d3a80e1bc63c293cc0a661d2fd8
Soham Datta <sohamdatta34@gmail.com>
2023-06-16 16:27:52 +0530

    added profile image to topbar

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
+import { auth } from '@/firebase/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 type Props = {};
 
 const Topbar: React.FC<Props> = () => {
+    const [user] = useAuthState(auth);
     return (
         <div className="flex items-center justify-between sm:px-12 px-2 md:px-24 bg-dark-layer-1">
             <Link href="/" className="flex items-center justify-center h-20">
@@ -12,14 +16,20 @@ const Topbar: React.FC<Props> = () => {
             <div className="flex items-center justify-center gap-4 sm:gap-8">
                 <a href="https://github.com/sohamdata/elitecode/" target="_blank" rel="noopener noreferrer">
                     <button
-                        className="bg-gray-700 px-2 py-1 sm:px-4 rounded-lg text-white text-sm font-medium hover:bg-brand-orange transition duration-300"
+                        className="bg-gray-700 px-2 py-1 sm:px-4 rounded-lg text-brand-orange text-sm font-medium hover:bg-brand-orange hover:text-white transition duration-300"
                     >Premium</button>
                 </a>
-                <Link href="/auth">
-                    <button
-                        className="bg-gray-700 px-2 py-1 sm:px-4 rounded-lg text-white text-sm font-medium hover:bg-white hover:text-brand-orange transition duration-300"
-                    >Sign In</button>
-                </Link>
+                {!user ?
+                    <Link href="/auth">
+                        <button
+                            className="bg-gray-700 px-2 py-1 sm:px-4 rounded-lg text-white text-sm font-medium hover:bg-white hover:text-brand-orange transition duration-300"
+                        >Sign In</button>
+                    </Link>
+                    :
+                    (
+                        <Image src="/pfp.png" alt="profile image" width={32} height={32} className="cursor-pointer hover:opacity-80 transition duration-300" />
+                    )
+                }
             </div>
         </div >
     )
