6c070a672450d2fa58108a82c16c5eb36cc55d0c
Soham Datta <sohamdatta34@gmail.com>
2023-06-21 21:42:37 +0530

    added react-hot-toast to login and app

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -4,6 +4,7 @@ import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/firebase';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 interface Props { };
 
@@ -39,13 +40,17 @@ const Login: React.FC<Props> = () => {
 
     const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (!password || !email) return alert("we need all fields to send you spam mails.");
+        if (!password || !email) {
+            toast.error('we need all fields to send you spam mails.');
+            return;
+        }
         try {
             const newUser = await signInWithEmailAndPassword(email, password);
             if (!newUser) return;
             router.push("/");
         } catch (error: any) {
-            alert(error.message);
+            // alert(error.message);
+            toast.error(error.message);
         }
     }
 
@@ -56,34 +61,36 @@ const Login: React.FC<Props> = () => {
     }, [error]);
 
     return (
-        <form className="space-y-4 px-4 pb-4" onSubmit={handleSignIn}>
-            <h1 className="text-center text-2xl text-white font-medium">Sign in to EliteCode</h1>
-            <div>
-                <label htmlFor="email" className="my-2 block text-white">Email</label>
-                <input type="email" name="email" id="email" placeholder="example@mail.com"
-                    className="p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400"
-                    onChange={handleInputChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="password" className="my-2 block text-white">Password</label>
-                <input type="password" name="password" id="password" placeholder="a uniquely insecure password"
-                    className="mb-4 p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400"
-                    onChange={handleInputChange}
-                />
-            </div>
-            <button type="submit" className="py-2 w-full rounded-md bg-brand-orange font-medium text-white hover:bg-white hover:text-brand-orange transition duration-300">
-                {loading ? "contacting the FBI..." : "Sign in"}
-            </button>
-            <button type="submit" className="w-full text-xs text-end text-white hover:text-brand-orange hover:underline"
-                onClick={handleClickForgotPassword}
-            >Forgor password?</button>
-            <div>
-                <p className="text-center text-white cursor-pointer">Don't have an account? <a className="text-brand-orange hover:underline"
-                    onClick={handleClickSignup}
-                >Sign up</a></p>
-            </div>
-        </form>
+        <>
+            <form className="space-y-4 px-4 pb-4" onSubmit={handleSignIn}>
+                <h1 className="text-center text-2xl text-white font-medium">Sign in to EliteCode</h1>
+                <div>
+                    <label htmlFor="email" className="my-2 block text-white">Email</label>
+                    <input type="email" name="email" id="email" placeholder="example@mail.com"
+                        className="p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400"
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password" className="my-2 block text-white">Password</label>
+                    <input type="password" name="password" id="password" placeholder="a uniquely insecure password"
+                        className="mb-4 p-1.5 rounded-md outline-none border-5 w-full text-black sm:text-sm placeholder-gray-400"
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <button type="submit" className="py-2 w-full rounded-md bg-brand-orange font-medium text-white hover:bg-white hover:text-brand-orange transition duration-300">
+                    {loading ? "contacting the FBI..." : "Sign in"}
+                </button>
+                <button type="submit" className="w-full text-xs text-end text-white hover:text-brand-orange hover:underline"
+                    onClick={handleClickForgotPassword}
+                >Forgor password?</button>
+                <div>
+                    <p className="text-center text-white cursor-pointer">Don't have an account? <a className="text-brand-orange hover:underline"
+                        onClick={handleClickSignup}
+                    >Sign up</a></p>
+                </div>
+            </form>
+        </>
     )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="EliteCode" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Toaster
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
       <Component {...pageProps} />
     </RecoilRoot>
   )
