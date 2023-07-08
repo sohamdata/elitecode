26261f373251521c182db6fc7c5bbb711730e748
Soham Datta <sohamdatta34@gmail.com>
2023-07-08 23:13:58 +0530

    feat: defined static paths and getStaticPaths

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,9 +1,13 @@
 import Topbar from "@/components/Topbar/Topbar";
 import Workspace from "@/components/Workspace/Workspace";
+import { problems } from "@/utils/problems";
+import { Problem } from "@/utils/types/problem";
 
-interface ProblemPageProps { };
+interface ProblemPageProps {
+    problem: Problem,
+};
 
-const ProblemPage = (props: ProblemPageProps) => {
+const ProblemPage = ({ problem }: ProblemPageProps) => {
     return (
         <>
             <Topbar problemPage />
@@ -13,3 +17,38 @@ const ProblemPage = (props: ProblemPageProps) => {
 }
 
 export default ProblemPage;
+
+export async function getStaticPaths() {
+    const paths = Object.keys(problems).map((key) => ({
+        params: { pid: key }
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+interface GetStaticPropsParams {
+    params: {
+        pid: string;
+    }
+}
+
+export async function getStaticProps({ params }: GetStaticPropsParams) {
+    const { pid } = params;
+    const problem = problems[pid];
+    problem.handlerFunction = problem.handlerFunction.toString();
+
+    if (!problem) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            problem
+        }
+    }
+}
