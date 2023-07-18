bac70b3ea74eab5d143d3d85b3cfc1777db9cc11
Soham Datta <sohamdatta34@gmail.com>
2023-07-19 01:09:17 +0530

    fix: splitwrapper error

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -19,14 +19,14 @@ const Workspace = ({ problem }: WorkspaceProps) => {
             <Split className="split" minSize={0}>
                 <Description problem={problem} />
                 <Playground problem={problem} onSuccess={setSuccess} />
-                {success && <div className="absolute z-10 top-0 left-0 w-full h-full bg-green-900 bg-opacity-20 flex justify-center items-center">
-                    <div className="bg-white rounded-lg p-5">
-                        <div className="text-2xl font-bold text-center">Congratulations!</div>
-                        <div className="text-center">You have successfully completed this problem</div>
-                    </div>
-                </div>
-                }
             </Split>
+            {success && <div className="absolute z-10 top-0 left-0 w-full h-full bg-green-900 bg-opacity-20 flex justify-center items-center">
+                <div className="bg-white rounded-lg p-5">
+                    <div className="text-2xl font-bold text-center">Congratulations!</div>
+                    <div className="text-center">You have successfully completed this problem</div>
+                </div>
+            </div>
+            }
         </>
     );
 }
