3f11875ce9495f2d71e416badab69df6fcae8f0e
Soham Datta <sohamdatta34@gmail.com>
2023-07-18 13:33:42 +0530

    feat: code editor fullscreen

--- src/components/Workspace/Playground/PreferenceNav.tsx
+++ src/components/Workspace/Playground/PreferenceNav.tsx
@@ -1,10 +1,24 @@
 import dynamic from "next/dynamic";
-import { AiOutlineExpandAlt, AiOutlineSetting } from "react-icons/ai";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { AiOutlineExpandAlt, AiOutlineSetting, AiOutlineShrink } from "react-icons/ai";
 const CustomTooltip = dynamic(() => import('../../CustomToolTip/CustomToolTip'), { ssr: false });
 
 interface PreferenceNavProps { };
 
 const PreferenceNav = (props: PreferenceNavProps) => {
+    const [isFullScreen, setIsFullScreen] = useState(false);
+
+    const handleFullScreen = () => {
+        if (isFullScreen) {
+            document.exitFullscreen();
+        }
+        else {
+            document.documentElement.requestFullscreen();
+        }
+        setIsFullScreen(!isFullScreen);
+    };
+
     return (
         <div className='pt-2 h-11 w-full flex justify-between items-center bg-dark-layer-2 overflow-x-hidden z-50'>
             <div className="px-3 py-1.5 bg-dark-layer-1 rounded-lg text-xs text-white font-medium hover:bg-neutral-700 cursor-pointer">
@@ -12,18 +26,22 @@ const PreferenceNav = (props: PreferenceNavProps) => {
             </div>
             <div className="mx-3 flex items-center space-x-2">
                 <div className="p-1 rounded-md text-slate-400 hover:bg-neutral-700 cursor-pointer">
-                    <CustomTooltip
-                        id="tooltip-settings"
-                        content="Settings"
-                        child={<AiOutlineSetting />}
-                    />
+                    <button onClick={() => toast.error('TODO settings')}>
+                        <CustomTooltip
+                            id="tooltip-settings"
+                            content="Settings"
+                            child={<AiOutlineSetting />}
+                        />
+                    </button>
                 </div>
                 <div className="p-1 rounded-md text-slate-400 hover:bg-neutral-700 cursor-pointer">
-                    <CustomTooltip
-                        id="tooltip-fs"
-                        content="FullScreen"
-                        child={<AiOutlineExpandAlt />}
-                    />
+                    <button onClick={handleFullScreen}>
+                        <CustomTooltip
+                            id="tooltip-fs"
+                            content="FullScreen"
+                            child={!isFullScreen ? <AiOutlineExpandAlt /> : <AiOutlineShrink />}
+                        />
+                    </button>
                 </div>
             </div>
         </div>
